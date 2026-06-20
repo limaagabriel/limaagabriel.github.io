6c2103f204976cdbc735b25139cafb19f35d03f8
Gabriel Lima <gabriel.lima@liferay.com>
2026-06-19 21:25:24 -0300

    Use my name and draft resume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from 'next'
 import { Lexend_Deca } from 'next/font/google'
+import clsx from 'clsx'
 
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
@@ -7,43 +8,42 @@ import { Layout } from '@/components/Layout'
 import '@/styles/tailwind.css'
 
 const lexendDeca = Lexend_Deca({
-	subsets: ['latin'],
-	variable: '--font-lexend-deca',
+  subsets: ['latin'],
+  variable: '--font-lexend-deca',
 })
 
 export const metadata: Metadata = {
-	title: {
-		template: '%s - Spencer Sharp',
-		default:
-			'Spencer Sharp - Software designer, founder, and amateur astronaut',
-	},
-	description:
-		'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
-	alternates: {
-		types: {
-			'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
-		},
-	},
+  title: {
+    template: '%s - Gabriel Lima',
+    default: 'Gabriel Lima - Software engineering',
+  },
+  description:
+    "I'm Gabriel, a software engineer and entrepeneur based in Recife. I'm focused on delivering technology while focusing on self-improvement.",
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
 }
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode
+  children: React.ReactNode
 }) {
-	return (
-		<html
-			lang="en"
-			className={`${lexendDeca.variable} h-full antialiased`}
-			suppressHydrationWarning
-		>
-			<body className="flex h-full bg-zinc-50 dark:bg-black">
-				<Providers>
-					<div className="flex w-full">
-						<Layout>{children}</Layout>
-					</div>
-				</Providers>
-			</body>
-		</html>
-	)
+  return (
+    <html
+      lang="en"
+      className={clsx(lexendDeca.variable, 'h-full antialiased')}
+      suppressHydrationWarning
+    >
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
 }
